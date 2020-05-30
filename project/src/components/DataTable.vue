ec3b512f86af7c2c833378bc21b6717fe69c171b
Nurzatilimani Muhamad Ahwan <zatilimaniahwan@gmail.com>
2020-05-30 15:20:11 +0800

    updated

--- project/src/components/DataTable.vue
+++ project/src/components/DataTable.vue
@@ -4,13 +4,52 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">Rate Page</h1>
-          <b-field position="is-right">
-            <b-input v-model="search_query" placeholder="Search" type="search" icon="magnify" />
-          </b-field>
+         
         </div>
       </div>
     </section>
     <div class="box is-8">
+        <div class="card">
+              <header class="card-header">
+    <p class="card-header-title">
+   <b-icon 
+   icon="fire"
+    size="is-small">
+   ></b-icon>&nbsp; Picked List
+    </p>
+  </header>
+  <div class="card-content">
+      <b-table :data="favorite">
+          <template slot-scope="props">
+            <b-table-column field="no" label="No" >{{ props.index+1 }}</b-table-column>
+            <b-table-column field="name" label="Name" >{{ props.row.name }}</b-table-column>
+            <b-table-column field="symbol" label="Symbol">{{ props.row.symbol }}</b-table-column>
+            <b-table-column field="symbol" label="Buy Rate">{{ props.row.rate }}</b-table-column>
+            <b-table-column field="symbol" label="Sell Rate">{{ props.row.sellRate }}</b-table-column>
+             <b-table-column field="removed" label=""  centered>
+              <b-field grouped>
+            <b-button size="is-medium" type="is-danger"  icon-left="minus-circle-outline" @click="unfavoriteCurrency(props.row.name)"></b-button>
+              </b-field>
+          </b-table-column>
+          </template>
+      </b-table>
+  </div>
+        </div>
+        <hr>
+        <div class="card">
+         <header class="card-header">
+            
+    <p class="card-header-title">
+      <b-icon 
+   icon="chart-bar"
+    size="is-small">
+   ></b-icon> &nbsp;Daily Rate
+    </p>
+     <b-field position="is-right" size="is-small">
+            <b-input v-model="search_query" placeholder="Search" type="search" icon="magnify" />
+          </b-field>
+  </header>
+  <div class="card-content">
       <b-table
         :data="filterItem"
         :paginated="isPaginated"
@@ -31,12 +70,13 @@
           <b-table-column field="sell-rate" label="Sell Rate" sortable>{{ props.row.sellRate }}</b-table-column>
           <b-table-column field="bookmark" label=""  centered>
               <b-field grouped>
-            <b-button size="is-medium" type="is-warning"  icon-left="star" @click="favoriteCurrency(props.row.rowIndex)"></b-button> &nbsp;
-            <b-button size="is-medium"  icon-left="star" @click="unfavoriteCurrency(props.row.index)"></b-button>
+            <b-button size="is-medium" type="is-warning"  icon-left="star" @click="favoriteCurrency(props.row.name,props.row.symbol,props.row.rate,props.row.sellRate)"></b-button>
               </b-field>
           </b-table-column>
         </template>
       </b-table>
+  </div>
+        </div>
     </div>
   </div>
 </template>
@@ -47,6 +87,7 @@ export default {
   data () {
     return {
       data: [],
+      favorite:[],
       isPaginated: true,
       paginationPosition: 'bottom',
       defaultSortDirection: 'asc',
@@ -54,8 +95,7 @@ export default {
       perPage: 10,
       sortIcon: 'arrow-up',
       search_query: '',
-      errors: [],
-     message:''
+      errors: []
     }
   },
   methods: {
@@ -68,19 +108,35 @@ export default {
             value.sellRate = value.sellRate.toFixed(5)
             value.margin = value.margin.toFixed(5)
             this.data.push(value)
-            
           })
         })
         .catch(e => {
           this.errors.push(e.data)
         })
     },
-    favoriteCurrency (row) {
-          alert("You choose to pinned"+" "+row)
-          
+    favoriteCurrency (name,symbol,rate,sellRate) {
+       let fav= {
+           'name': name,
+           'symbol':symbol,
+           'rate':rate,
+        'sellRate':sellRate
+       }
+       if(this.favorite.length >0){
+           if(this.favorite.some(favor=> favor.name == fav.name)) {
+               alert("Already pinned that currency. Choose the other one")
+            }
+            else{
+                this.favorite.push(fav)
+            }
+       }else{
+        this.favorite.push(fav)
+       }
+
+  
+        
     },
     unfavoriteCurrency (row) {
-        alert("You choose to unpinned"+" "+row)
+        this.favorite.splice(row,1);
     }
   },
   mounted () {
